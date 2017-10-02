29e9e3b4818f7061c43d6a2c51c6647ce2296dfa
amit-upadhyay-it <youarefreak1@gmail.com>
2017-10-03 01:39:51 +0530

    long search

--- modules/get_files.js
+++ modules/get_files.js
@@ -9,7 +9,18 @@ getFiles();
 
 function getFiles()
 {
+/*
     find.file(fileNames[0], './../', function(files) {
         console.log(files);
     });
+*/
+
+    find.eachfile(fileNames[0], './../', function(file) {
+        console.log(file);
+        if (file.indexOf('node_modules') !== -1)// the directory where search is going is under node_modules
+            console.log('fuck');
+    })
+        .end(function() {
+            console.log('Ended');
+        });
 }
