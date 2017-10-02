bc28fcb8e2270196d46ce1d62e0a6b5ba33f3938
amit-upadhyay-it <youarefreak1@gmail.com>
2017-10-02 18:27:44 +0530

    commit checking

--- modules/match_command.js
+++ modules/match_command.js
@@ -1,4 +1,5 @@
 
+// the input to be passed here is array of strings
 var matchPushCmd = function (inputArray) {
 
     var inputCmd = '';
@@ -7,11 +8,32 @@ var matchPushCmd = function (inputArray) {
     {
         inputCmd = inputCmd + inputArray[i] + ' ';
     }
+    inputCmd = inputCmd.slice(0, -1);//slicing the last space.
 
-    inputCmd = inputCmd.slice(0, -1);
-
-    return (inputCmd === 'git push -u origin master' || inputCmd === 'git push origin master');
+    var toMatchCmd1 = 'git push -u origin master';
+    var toMatchCmd2 = 'git push origin master';
+    return (inputCmd === toMatchCmd1 || inputCmd === toMatchCmd2);
 }
 
-var val = matchPushCmd(process.argv.slice(2));
-console.log(val);
+
+// the input to be passed here is array of strings
+// returns null of not matched otherwise returns []
+var matchCommitCmd = function (inputArray) {
+
+    var inputCmd = '';
+    // cancatenate the elements of inputArray with space included after each word
+    for (var i = 0; i < inputArray.length; ++i)
+    {
+        inputCmd = inputCmd + inputArray[i] + ' ';
+    }
+    inputCmd = inputCmd.slice(0, -1);//slicing the last space.
+
+    var toMatchCmd1 = 'git clone *';
+    return inputCmd.match(toMatchCmd1);
+}
+
+var returnObj = matchCommitCmd(process.argv.slice(2));
+if (returnObj !== null)
+    console.log(returnObj.input);
+else
+    console.log('Invalid command');
