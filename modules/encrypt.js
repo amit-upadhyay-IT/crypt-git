1b11492e30ba5745041e18fd72f2acb66fa3ce35
amit-upadhyay-it <youarefreak1@gmail.com>
2017-10-02 23:33:39 +0530

    modulel load

--- modules/encrypt.js
+++ modules/encrypt.js
@@ -2,12 +2,18 @@ var crypto = require('crypto');
 var algorithm = 'aes-256-ctr';
 
 var readline = require('readline');
+var stream = require('stream');
+var fs = require('fs');
+var zlib = require('zlib');
+
+
+// accepting password
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.setPrompt('\nEnter password: ');
+rl.setPrompt('\nEnter encryption password: ');
 rl.prompt();
 rl.on('line', function(text) {
-    if (text.length > 5)
+    if (text.length > 5 && text.split(' ').length == 1)// split is checking if the password has space or not
     {
         rl.close();
     }
@@ -15,3 +21,5 @@ rl.on('line', function(text) {
 }).on('close', function() {
     process.exit(0);
 });
+
+
